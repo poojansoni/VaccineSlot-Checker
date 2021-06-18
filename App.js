bcaaeda79a35f0a6ac07a4ffd38247a5e5c14a49
Poojan <poojansoni.udr@gmail.com>
2021-06-18 21:47:39 +0530

    Completed Drawer Navigation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import "react-native-gesture-handler";
 import React from "react";
+import Icon from "react-native-vector-icons/Ionicons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
@@ -7,29 +8,74 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./src/screens/Home";
 import DetailsScreen from "./src/screens/Details";
 
-const Stack = createStackNavigator();
+const HomeStack = createStackNavigator();
+const DetailsStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
+
+const HomeStackScreen = ({ navigation }) => (
+	<HomeStack.Navigator
+		initialRouteName="Home"
+		screenOptions={{
+			headerStyle: { backgroundColor: "#774c60" },
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				fontWeight: "bold",
+			},
+		}}
+	>
+		<HomeStack.Screen
+			name="Home"
+			component={HomeScreen}
+			options={{
+				title: "Check your slot",
+				headerLeft: () => (
+					<Icon.Button
+						name="ios-menu"
+						size={25}
+						backgroundColor="#774c60"
+						onPress={() => navigation.openDrawer()}
+					/>
+				),
+			}}
+		/>
+	</HomeStack.Navigator>
+);
+
+const DetailsStackScreen = ({ navigation }) => (
+	<DetailsStack.Navigator
+		initialRouteName="Details"
+		screenOptions={{
+			headerStyle: { backgroundColor: "#774c60" },
+			headerTintColor: "#fff",
+			headerTitleStyle: {
+				fontWeight: "bold",
+			},
+		}}
+	>
+		<DetailsStack.Screen
+			name="Details"
+			component={DetailsScreen}
+			options={{
+				headerLeft: () => (
+					<Icon.Button
+						name="ios-menu"
+						size={25}
+						backgroundColor="#774c60"
+						onPress={() => navigation.openDrawer()}
+					/>
+				),
+			}}
+		/>
+	</DetailsStack.Navigator>
+);
 
 const App = () => {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator
-				initialRouteName="Home"
-				screenOptions={{
-					headerStyle: { backgroundColor: "#774c60" },
-					headerTintColor: "#fff",
-					headerTitleStyle: {
-						fontWeight: "bold",
-					},
-				}}
-			>
-				<Stack.Screen
-					name="Home"
-					component={HomeScreen}
-					options={{ title: "Check your slot" }}
-				/>
-				<Stack.Screen name="Details" component={DetailsScreen} />
-			</Stack.Navigator>
+			<Drawer.Navigator initialRouteName="Home">
+				<Drawer.Screen name="Home" component={HomeStackScreen} />
+				<Drawer.Screen name="Details" component={DetailsStackScreen} />
+			</Drawer.Navigator>
 		</NavigationContainer>
 	);
 };
